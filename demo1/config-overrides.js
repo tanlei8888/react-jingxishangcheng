7e143c6bfd42ade1fe017033480a7997d4146dd6
zelin <zelin@zelindeMacBook-Pro-35.local>
2020-06-28 09:58:39 +0800

    shangchu

--- demo1/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra');
-module.exports = override(
-  ...addBabelPlugins(
-    [
-      "styled-jsx/babel",
-    //   { "plugins": ["styled-jsx-plugin-less"] }
-    ]
-  )
-)
